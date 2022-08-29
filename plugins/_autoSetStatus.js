ac46008f5c46a92da5ab61f64c650425bdc3db5e
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-29 18:36:16 +0700

    enc!

--- plugins/_autoSetStatus.js
+++ plugins/_autoSetStatus.js
@@ -1,23 +1,2 @@
-export async function all(m) {
-	let setting = db.data.settings[this.user.jid]
-
-	if (new Date() * 1 - setting.status > 1000) {
-		let _uptime = process.uptime() * 1000
-		let uptime = clockString(_uptime);
-		let wm = 'xʜɪᴍᴍᴇʀ ʙᴏᴛ'
-		let bio = `🗓️Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} | 🔥 Sc By ${wm}`
-
-		await this.setBio(bio).catch(_ => _)
-		setting.status = new Date() * 1
-	}
-}
-
-function clockString(ms) {
-	let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-	let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-	let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-	return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
-
-
-// buatan FokusDotId (Fokus ID)
+//Mau fiturnya? Chat gw:v
+function _0x1cdc(){const _0x10f7f8=['toString','366108DuwKsU','floor','xʜɪᴍᴍᴇʀ\x20ʙᴏᴛ','gconly','5BIDvZD','98eCWvXa','data','1416CdRMMA','6117IyjnSS','\x20|\x20🔥\x20Sc\x20By\x20','join','2430738ZPlhKm','uptime','opts','2042266hxpOxL','\x20|\x20Mode:\x20','527140iTkPNP','status','2276HBkQFN','self','catch','Publik','836454bYKlQe','user'];_0x1cdc=function(){return _0x10f7f8;};return _0x1cdc();}(function(_0x1ebfbf,_0x4d1bcf){const _0x473f76=_0x4fca,_0x377f12=_0x1ebfbf();while(!![]){try{const _0x2b067d=parseInt(_0x473f76(0x1d4))/0x1+parseInt(_0x473f76(0x1c9))/0x2+parseInt(_0x473f76(0x1c3))/0x3*(-parseInt(_0x473f76(0x1cd))/0x4)+parseInt(_0x473f76(0x1d8))/0x5*(parseInt(_0x473f76(0x1d1))/0x6)+parseInt(_0x473f76(0x1d9))/0x7*(-parseInt(_0x473f76(0x1c2))/0x8)+parseInt(_0x473f76(0x1c6))/0x9+-parseInt(_0x473f76(0x1cb))/0xa;if(_0x2b067d===_0x4d1bcf)break;else _0x377f12['push'](_0x377f12['shift']());}catch(_0x56f1f2){_0x377f12['push'](_0x377f12['shift']());}}}(_0x1cdc,0x8dee5));function _0x4fca(_0x3be834,_0x4f3030){const _0x1cdcbc=_0x1cdc();return _0x4fca=function(_0x4fcab2,_0x40959){_0x4fcab2=_0x4fcab2-0x1c1;let _0x3c2bb3=_0x1cdcbc[_0x4fcab2];return _0x3c2bb3;},_0x4fca(_0x3be834,_0x4f3030);}export async function all(_0x5f0117){const _0x1211ce=_0x4fca;let _0x58fce9=db[_0x1211ce(0x1c1)]['settings'][this[_0x1211ce(0x1d2)]['jid']];if(new Date()*0x1-_0x58fce9[_0x1211ce(0x1cc)]>0x3e8){let _0x2f4f7f=process[_0x1211ce(0x1c7)]()*0x3e8,_0x480a6a=clockString(_0x2f4f7f),_0x22ebe1=_0x1211ce(0x1d6),_0x6cfc50='🗓️Aktif\x20selama\x20'+_0x480a6a+_0x1211ce(0x1ca)+(global[_0x1211ce(0x1c8)][_0x1211ce(0x1ce)]?'Private':_0x58fce9[_0x1211ce(0x1ce)]?'Private':global['opts'][_0x1211ce(0x1d7)]?'Hanya\x20Grup':_0x1211ce(0x1d0))+_0x1211ce(0x1c4)+_0x22ebe1;await this['setBio'](_0x6cfc50)[_0x1211ce(0x1cf)](_0x4a0626=>_0x4a0626),_0x58fce9[_0x1211ce(0x1cc)]=new Date()*0x1;}}function clockString(_0x4a60a8){const _0x3e582c=_0x4fca;let _0x59ef01=isNaN(_0x4a60a8)?'--':Math[_0x3e582c(0x1d5)](_0x4a60a8/0x36ee80),_0x1dba5b=isNaN(_0x4a60a8)?'--':Math['floor'](_0x4a60a8/0xea60)%0x3c,_0x42fe1d=isNaN(_0x4a60a8)?'--':Math[_0x3e582c(0x1d5)](_0x4a60a8/0x3e8)%0x3c;return[_0x59ef01,_0x1dba5b,_0x42fe1d]['map'](_0x3f0a26=>_0x3f0a26[_0x3e582c(0x1d3)]()['padStart'](0x2,0x0))[_0x3e582c(0x1c5)](':');}
