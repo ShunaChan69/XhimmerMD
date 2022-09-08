8138715ce5d063a1fe86ae2fb94567cacce4ca43
Shioo <110561925+ShionMDv@users.noreply.github.com>
2022-09-08 23:39:25 +0700

    Update _menfess.js

--- plugins/_menfess.js
+++ plugins/_menfess.js
@@ -4,7 +4,7 @@ export async function before(m, { conn, isOwner }) {
 if (/(MENFESSIN)!/i.test(m.quoted.text)) {
         if (!m.text) return m.reply('Harap gunakan teks untuk membalas pesan rahasia!')
         m.react('💌')
-        let med = (["https://telegra.ph/file/620bcc109157b23a656f3.jpg", "https://telegra.ph/file/3eb3c21b4d19626332284.jpg"]).getRandom()
+        let med = (["https://telegra.ph/file/d81248e85aa7b3dde35b2.jpg", "https://telegra.ph/file/d81248e85aa7b3dde35b2.jpg"]).getRandom()
         let { data } = await conn.getFile(med)
         conn.reply(m.quoted.mentionedJid[0], `*MENFESSIN!*\n\nBalasan pesan menfess\n\nPesan : ${m.text}\n\n_Gesek pesan ini kekanan untuk mengirim balasan!_`, 0, {
             ephemeralExpiration: 86400,
