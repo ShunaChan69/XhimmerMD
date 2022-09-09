8a0e01b3620a260708f5eb37af303d86d84424da
Shioo <110561925+ShionMDv@users.noreply.github.com>
2022-09-09 15:29:19 +0700

    xʜɪᴍᴍᴇʀ ʙᴏᴛ࿐

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,42 +1,114 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `*Ini Kak owner bot nya*`
+let { MessageType } = (await import('@adiwajshing/baileys')).default
 
-let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-const message = {
-            document: { url: thumbdoc },
-            jpegThumbnail: await (await fetch(thumbdoc)).buffer(), fileName: global.wm2, mimetype: td, fileLength: '9999999', pageCount: '999',
-            caption: titlebot,
-            footer: wm,
-            templateButtons: [
-                {
-                    urlButton: {
-                        displayText: 'IG OWNER',
-                        url: 'https://instagram.com/_alv.stn',
-                    }
-                },
-                {
-                    urlButton: {
-                        displayText: 'Author',
-                        url: 'https://wa.me/6287734910547',
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+  let type = (args[0] || '').toLowerCase()
+  let _type = (args[0] || '').toLowerCase()
 
-                    }
-                },
-                {
-                    quickReplyButton: {
-                        displayText: 'OKE',
-                        id: 'Ok'
-                    }
-                },
-            ]
-        }
-        conn.sendMessage(m.chat, message)
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Jangan di spam kak owner ku', sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
+//------- NOMOR
+  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *OWNER* ${htka}
+• @${nomorown.split`@`[0]} •
+------- ${nameown} -------
+📮 *Note:*
+• Owner tidak menerima save contact
+• Owner berhak blockir tanpa alasan
+• Berbicaralah yang sopan & tidak spam
+• Owner Hanya merespon yang berkaitan dengan BOT
+• No Telp
+• Chat gajelas = Block`
+
+//------------ BIO
+let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
+let teksbio = `${htki} *BIODATA* ${htka}
+*💌 Nama* : Vinz
+*✉️ Nama RL* : VinzCyx
+*♂️ Gender* : Laki - laki
+*🕋 Agama* : Islam
+*⏰ Tanggal lahir* : 15 Maret 2004
+*🎨 Umur* : 18
+*🧮 Kelas* : Dah Tamat
+*🧩 Hobby* : Nonton henta*, main game, Recode script
+*💬 Sifat* : Baik, hode, softboy, tydack ramah, toxic wwkwk
+*🗺️ Tinggal* : Indonesia, Jawa Tengah, Demak
+*❤️ Suka* : warnah pink & hitam, trap, waifu wangy, kucing
+*💔 Benci* : anak alay, autis, anak epep, sok asik
+*- - sᴋɪʟʟs: - -* 
+> JavaScript [70.7%]
+> Python [11.4%]
+> CSS [4.1%]
+> Html [0.9%]
+> Recode [76.8%]
+> Create Webs [88.9%]
+───────[ SOSIAL MEDIA ]───────
+📷 *Instagram:* ${sig}
+🐈 *Github:* ${sgh}
+🥏 *Whatsapp* wa.me/${nomorown}
+🌐 *Discord:* Soon
+🌏 *Website:* Gaada
+🎶 *Tiktok:* tiktok.com/wvinz
+`
+  let teks = ' '
+const sections = [
+   {
+	title: `${htjava} OWNER –––––––––·•`,
+	rows: [
+	    {title: "📱 • Nomor", rowId: ".owner nomor"},
+	{title: "🎨 • Biodata", rowId: ".owner bio"},
+	{title: "🌐 • Website", rowId: ".website"},
+	{title: "🌎 • Script", rowId: ".sc"},
+	{title: "🏮 • Youtube", rowId: ".fyt"},
+	]
+    },{
+	title: `${htjava} SUPPORT ME –––––––·•`,
+	rows: [
+	    {title: "💹 • Donasi", rowId: ".donasi"},
+	{title: "🔖 • Sewa", rowId: ".sewa"},
+	{title: "🌟 • Buy Premium", rowId: ".premium"},
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `${htki} *OWNER* ${htka}`,
+  buttonText: "Click Here !",
+  sections
 }
-handler.command = /^(vinz)$/i
 
-handler.register = true
-handler.exp = 3
+  try {
+    if (/(creator|owner)/i.test(command)) {
+      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+        switch (type) {
+          case 'nomor':
+          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
+            break
+            case 'bio':
+          conn.sendHydrated(m.chat, teksbio, wm, ppown, "https://wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+            break
+            
+          default:
+            return await conn.sendMessage(m.chat, listMessage, { quoted: m, contextInfo: { mentionedJid: [m.sender] }})
+        }
+    } else if (/aoaooaoaooaoa/i.test(command)) {
+      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+      switch (_type) {
+        case 't':
+          break
+        case '':
+          break
+
+        default:
+          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
+      }
+    }
+  } catch (err) {
+    m.reply("Error\n\n\n" + err.stack)
+  }
+}
+
+handler.help = ['owner', 'creator']
+handler.tags = ['main', 'info']
+handler.command = /^(owner|creator)/i
 
 export default handler
